1e5e4a125236dacd11b64379fe4f1d448ab1b4b5
ThiNguyen <thi.nguyen@fmgsuite.com>
2025-05-27 21:05:47 +0700

    1st commit

---
+++ SimpleWebApi/Controllers/CompanyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebApi.Controllers
+{
+    [Route("api/company")]
+    public class CompanyController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from TestControllerfhghfgh!";
+        }
+    }
+}
